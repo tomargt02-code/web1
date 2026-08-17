4183b4fe5aa9674beb09dcd91e6f0d38eaefcbde
tomargt02-code <tomargt02@gmail.com>
2026-08-17 04:56:33 +0000

    Done

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -58,8 +58,8 @@ export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
 
-import type { getRouter } from './router.tsx'
-import type { startInstance } from './start.ts'
+import type { getRouter } from '../router.tsx'
+import type { startInstance } from '../start.ts'
 declare module '@tanstack/react-start' {
   interface Register {
     ssr: true
